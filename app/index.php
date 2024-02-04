c0c5a5989d8f23735fb3872399d4372f679414a0
Daniël Hofman <d.hofman@st.hanze.nl>
2024-02-04 16:11:40 +0100

    Dropdown fix

--- app/index.php
+++ app/index.php
@@ -21,6 +21,7 @@
     $to = array_unique($to);
     if (!count($to)) $to[] = '0,0';
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -128,14 +129,18 @@
             <select name="piece">
                 <?php
                     foreach ($hand[$player] as $tile => $ct) {
-                        echo "<option value=\"$tile\">$tile</option>";
+                        if ($ct > 0) {
+                            echo "<option value=\"$tile\">$tile</option>";
+                        }
                     }
                 ?>
             </select>
             <select name="to">
                 <?php
                     foreach ($to as $pos) {
-                        echo "<option value=\"$pos\">$pos</option>";
+                        if (AvailablePositions($board, $hand[$player], $player, $pos)) {
+                            echo "<option value=\"$pos\">$pos</option>";
+                        }
                     }
                 ?>
             </select>
@@ -145,6 +150,9 @@
             <select name="from">
                 <?php
                     foreach (array_keys($board) as $pos) {
+                        if($board[$pos][0][0] != $player) {
+                            continue;
+                        }
                         echo "<option value=\"$pos\">$pos</option>";
                     }
                 ?>
